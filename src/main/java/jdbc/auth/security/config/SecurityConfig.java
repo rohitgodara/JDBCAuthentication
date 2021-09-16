5c4b32104e3bd5d582c7990aee93f315c5f8d6b0
Rohit Godara <rohit.godara@magicsw.com>
2021-09-16 13:13:03 +0530

    updated formlogin with http basic auth

--- src/main/java/jdbc/auth/security/config/SecurityConfig.java
+++ src/main/java/jdbc/auth/security/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,28 +18,33 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	public PasswordEncoder encoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
 	@Autowired
 	private UserDetailsService userDetailsService;
-	
-	//It creates an authProvider strategy
+
+	// It creates an authProvider strategy
 	@Bean
 	public DaoAuthenticationProvider authProvider() {
-	    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-	    authProvider.setUserDetailsService(userDetailsService);
-	    authProvider.setPasswordEncoder(encoder());
-	    return authProvider;
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+		authProvider.setUserDetailsService(userDetailsService);
+		authProvider.setPasswordEncoder(encoder());
+		return authProvider;
 	}
-	
-	//To set authProvider
+
+	// To set authProvider
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.authenticationProvider(authProvider());
 	}
-	
-//	@Override
-//	protected void configure(HttpSecurity http) throws Exception {
-//
-//	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http  
+        .authorizeRequests()  
+            .anyRequest().authenticated()  
+            .and()  
+        .formLogin().and()  
+        .httpBasic();  
+	}
 
 }
